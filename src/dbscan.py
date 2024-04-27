396a400f7da713e10c1a2e7a7ee691e00f18ac44
Rany Kamel <rany@ranykamel.dev>
2024-04-26 21:52:44 -0400

    fixed that annoying bug

--- src/dbscan.py
+++ src/dbscan.py
@@ -1,17 +1,30 @@
 import numpy as np
 
 def region_query(X: np.ndarray, P, eps):
+    '''
+    region_query: Find all points in X that are within a distance of eps from point P
+
+    Inputs:
+    X: np.ndarray: The dataset
+    P: np.ndarray: The point to find neighbors for
+    eps: float: The radius of the neighborhood
+
+    Returns:
+    neighbors: list: A list of indices of the points in X that are in the neighborhood of P
+    '''
+
     neighbors = []
     for i in range(X.shape[0]):
         if np.linalg.norm(X[i] - P) < eps:
             neighbors.append(i)
     return neighbors
 
-def expand_cluster(X: np.ndarray, P, cl_id, eps, min_pts, cluster_info = None):
+
+def expand_cluster(X: np.ndarray, P, cl_id, eps, min_pts, cluster_info = None, region_query = region_query):
     if cluster_info is None:
         cluster_info = np.zeros(X.shape[0])
 
-    seeds = region_query(X, X[P], eps)
+    seeds = region_query(X, X[P], eps) # All points within eps distance from P
     if len(seeds) < min_pts: # not a core point
         return False
     else: # all points in seeds are density reachable from P
@@ -20,7 +33,7 @@ def expand_cluster(X: np.ndarray, P, cl_id, eps, min_pts, cluster_info = None):
         seeds.remove(P)
         while len(seeds) > 0:
             current_point = seeds[0]
-            result = region_query(X, current_point, eps)
+            result = region_query(X, X[current_point], eps)
             
             if len(result) >= min_pts:
                 for i in range(len(result)):
@@ -33,7 +46,7 @@ def expand_cluster(X: np.ndarray, P, cl_id, eps, min_pts, cluster_info = None):
         return True
     
 
-def dbscan(X: np.ndarray, eps, min_pts, cluster_info = None):
+def dbscan(X: np.ndarray, eps, min_pts, cluster_info = None, region_query = region_query):
     if cluster_info is None:
         cluster_info = np.zeros(X.shape[0])
         
@@ -45,7 +58,7 @@ def dbscan(X: np.ndarray, eps, min_pts, cluster_info = None):
     for i in range(X.shape[0]):
         point = X[i]
         if cluster_info[i] == UNCLASSIFIED:
-            cluster = expand_cluster(X, i, cluster_id, eps, min_pts, cluster_info)
+            cluster = expand_cluster(X, i, cluster_id, eps, min_pts, cluster_info, region_query)
             if cluster:
                 cluster_id += 2 if cluster_id == NOISE else 1
     return cluster_info
